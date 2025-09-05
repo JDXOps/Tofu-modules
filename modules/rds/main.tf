95352bff983f904410adcde24584c9dd7428509c
Jasdeep Singh <jsd@Jasdeeps-MacBook-Pro.local>
2025-09-05 16:08:30 +0100

    Add initial RDS module code

---
+++ modules/rds/main.tf
@@ -0,0 +1,25 @@
+resource "aws_db_subnet_group" "subnet_group" {
+  name       = "${var.identifier}-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = var.tags
+}
+
+
+resource "aws_db_instance" "db_instance" {
+  identifier            = var.identifier
+  allocated_storage     = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  db_subnet_group_name  = aws_db_subnet_group.subnet_group.id
+  deletion_protection   = var.enable_deletion_protection
+  engine                = var.engine
+  engine_version        = var.engine_version
+  instance_class        = var.instance_class
+  multi_az              = var.enable_multi_az
+  username              = var.master_user_password
+  password              = var.master_user_password
+  option_group_name     = var.option_group_name
+  parameter_group_name  = var.parameter_group_name
+  port                  = var.port
+  tags                  = var.tags
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,84 @@
+variable "identifier" {
+  type        = string
+  description = "The name of the RDS instance."
+}
+
+variable "allocated_storage" {
+  type        = number
+  description = "The allocated storage in GB."
+}
+
+variable "subnet_ids" {
+  type        = list(str)
+  description = "List of Subnet Ids that are for the RDS instance in a VPC."
+
+}
+
+variable "max_allocated_storage" {
+  type        = number
+  description = "Max Storage in GB the RDS instance is allowed to scale to."
+}
+
+variable "enable_deletion_protection" {
+  type        = bool
+  description = "The database can't be deleted if this value is set to true"
+  default     = true
+}
+
+variable "engine" {
+  type        = string
+  description = "The databsae engine to use for this DB instance."
+}
+
+variable "engine" {
+  type        = string
+  description = "The databsae engine to use for this DB instance."
+}
+
+variable "engine_version" {
+  type        = string
+  description = "The engine version to use."
+}
+
+variable "instance_class" {
+  type        = string
+  description = "The instance type of the RDS instance."
+}
+
+variable "enable_multi_az" {
+  type        = bool
+  description = "Specifies if the RDS instance is multi az."
+}
+
+variable "master_user_username" {
+  type        = string
+  description = "The username for the master db user."
+}
+
+variable "master_user_password" {
+  type        = string
+  description = "The password for the master db user."
+}
+
+variable "port" {
+  type        = number
+  description = "The port the DB accepts connections on."
+}
+
+variable "option_group_name" {
+  type        = string
+  description = "The name of the DB option group"
+  default     = null
+}
+
+variable "parameter_group_name" {
+  type        = string
+  description = "The name of the DB parameter group"
+  default     = null
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "The password for the master db user."
+}
+
